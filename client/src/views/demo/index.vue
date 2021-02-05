e07fe82485fcf3f6c9eea8e8859bd36efdfd7b65
王钱土 <wangqiantu@wangqiantudeMacBook-Pro.local>
2021-02-05 15:21:37 +0800

    client

---
+++ client/src/views/demo/index.vue
@@ -0,0 +1,24 @@
+<template>
+      <div>
+        <h1>Index</h1>
+      </div>
+    </template>
+
+    <!-- Vue -->
+    <script>
+    export default {
+      data() {
+        return {
+
+        }
+      },
+      methods: {
+
+      }
+    }
+    </script>
+
+    <!-- CSS样式 -->
+    <style scoped>
+
+    </style>
